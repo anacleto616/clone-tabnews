386e99bc76f663bf989c330d335a874334011109
Rafael Anacleto <rafael.anacleto12@gmail.com>
2026-01-19 19:24:18 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -33,6 +33,7 @@ describe("POST /api/v1/users", () => {
         username: "ranacleto",
         email: "ranacleto@email.com",
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
