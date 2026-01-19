d88f66ab59f483939f9272f8f48def66ca815100
Rafael Anacleto <rafael.anacleto12@gmail.com>
2026-01-19 20:43:16 -0300

    feat: add default feature `read:activation_token` when creating `user`

---
+++ tests/integration/_use-cases/resgistration-flow.test.js
@@ -0,0 +1,49 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Resgistration Flow (all successful)", () => {
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration.flow@email.com",
+          password: "senha123",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(201);
+
+    const createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "RegistrationFlow",
+      email: "registration.flow@email.com",
+      password: createUserResponseBody.password,
+      features: ["read:activation_token"],
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {});
+
+  test("Activate account", async () => {});
+
+  test("login", async () => {});
+
+  test("Get user information", async () => {});
+});
