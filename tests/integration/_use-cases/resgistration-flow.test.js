8b8c942f3535c4a795f2bd666be09b2e15b118ba
Rafael Anacleto <rafael.anacleto12@gmail.com>
2026-02-08 19:13:03 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/resgistration-flow.test.js
+++ tests/integration/_use-cases/resgistration-flow.test.js
@@ -84,7 +84,29 @@ describe("Use case: Resgistration Flow (all successful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("login", async () => {});
+  test("Login", async () => {
+    const createSessionsResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@email.com",
+          password: "senha123",
+        }),
+      },
+    );
+
+    console.log(createSessionsResponse);
+
+    expect(createSessionsResponse.status).toBe(201);
+
+    const createSessionsResponseBody = await createSessionsResponse.json();
+
+    expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
 
   test("Get user information", async () => {});
 });
