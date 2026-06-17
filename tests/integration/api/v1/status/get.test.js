722b5eb7bd3be2a7988f465c2e1edc66a23c9a44
Rafael Anacleto <rafael.anacleto12@gmail.com>
2026-06-16 21:34:15 -0300

    test: add default user coverage for `GET /api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -50,4 +50,30 @@ describe("GET /api/v1/status", () => {
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
   });
+
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const createdUser = await orchestrator.createUser();
+      const activatesUser = await orchestrator.activateUser(createdUser);
+      const defaultSessionUser =
+        await orchestrator.createSession(activatesUser);
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          Cookie: `session_id=${defaultSessionUser.token}`,
+        },
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
 });
